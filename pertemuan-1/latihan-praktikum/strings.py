fe14d5bf0e7c02af812b5bc3a86a953b1ce78f95
yohanes-Tambunan <yohanes.doly4585@student.unri.ac.id>
2026-02-01 20:49:33 +0700

    strings.py

---
+++ pertemuan-1/latihan-praktikum/strings.py
@@ -0,0 +1,14 @@
+#string dalam python dikelilingi oleh tanda kutip tunggal atau ganda
+#string adalah karakter yang digunakan untuk menangani data tekstual
+#kita dapat menampilkan string dengan fungsi print()
+
+#contoh
+print ("salam ")
+print ('salam')
+
+#Anda dapat menetapkan string multibaris ke variabel dengan menggunakan tiga tanda kutip
+#contoh
+a = """saya suka bermain catur,
+saya suka bermain bola,
+saya suka belajar teknik informatika."""
+print (a)
--- pertemuan-1/latihan-praktikum/tempCodeRunnerFile.py
+++ pertemuan-1/latihan-praktikum/tempCodeRunnerFile.py
@@ -1,32 +1,14 @@
-#casting (konversi tipe) adalah proses mengubah nilai dari satu tipe data ke tipe data lain. 
-# Hal ini dilakukan menggunakan fungsi bawaan yang sesuai dengan tipe yang diinginkan, seperti int(), float(), dan str()
-#python menyediakan beberapa fungsi bawaan untuk konversi tipe data secara eksplisit
-#int(x): Mengonversi x menjadi bilangan bulat. Jika x berupa bilangan pecahan (float), bagian desimalnya dihilangkan (dipotong), dan jika berupa string, maka harus berupa bilangan bulat.
-#float(x): Mengonversi x menjadi bilangan floating-point. Menerima bilangan bulat, bilangan floating-point, atau string yang mewakili angka yang valid.
-#str(x): Mengonversi x menjadi representasi string. Fungsi ini bekerja pada berbagai macam tipe data.
+#string dalam python dikelilingi oleh tanda kutip tunggal atau ganda
+#string adalah karakter yang digunakan untuk menangani data tekstual
+#kita dapat menampilkan string dengan fungsi print()
 
-#int
-x = int (1) #x akan menjadi 1
-y = int (2.8) #y akan menjadi 2
-z = int ("3") # z will be 3
-print(x)
-print(y)
-print(z)
+#contoh
+print ("salam ")
+print ('salam')
 
-#float
-x = float(1)     # x will be 1.0
-y = float(2.8)   # y will be 2.8
-z = float("3")   # z will be 3.0
-w = float("4.2") # w will be 4.2
-print(x)
-print(y)
-print(z)
-print (w)
-
-#string
-x = str("s1") # x will be 's1'
-y = str(2)    # y will be '2'
-z = str(3.0)  # z will be '3.0'
-print(x)
-print(y)
-print(z)
+#Anda dapat menetapkan string multibaris ke variabel dengan menggunakan tiga tanda kutip
+#contoh
+a = """saya suka bermain catur,
+saya suka bermain bola,
+saya suka belajar teknik informatika."""
+print (a)
