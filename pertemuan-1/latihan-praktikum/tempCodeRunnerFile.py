0b9be4e24e184bdd691ab6538f8cf1334a42c075
yohanes-Tambunan <yohanes.doly4585@student.unri.ac.id>
2026-02-01 17:37:24 +0700

    output.py

--- pertemuan-1/latihan-praktikum/tempCodeRunnerFile.py
+++ pertemuan-1/latihan-praktikum/tempCodeRunnerFile.py
@@ -1,19 +1,18 @@
-#1. syntax pada python
-#syntax adalah aturan penulisan kode yang dikenal bersih, sederhana, dan mudah dibaca, dengan fokus utama pada penggunaan indentasi (spasi/tab) untuk menentukan blok kode daripada kurung kurawal.
-#didalam python, indentasi sangat penting yang mengacu pada spasi di awal baris kode.
-if 10 > 5
-    print("Sepuluh lebih bedar dari lima")
+#1. cara menampilkan teks pada python
+#kita dapat menggunakan fungsi print () untuk menampilkan teks atau nilai keluaran
+print("hello world!")
+print("hello mars!")
+#teks harus berada diantara simbol "" ataupun simbol '
+print("makanan")
+print('makanan')
 
-#2. pernyataan pada python
-#pernyataan adalah baris kode yang dieksekusi oleh interpreter python.
-print("kamu terlihat manis")
-#di python, kita dapat menulis beberapa pernyataan dalam satu baris dengan memisahkannya menggunakan titik koma (;).
+#jika kita ingin menampilkan nilai keluaran sebaris,kita bisa menggunakan "end"
+print("aku suka kamu", end=" ")
+print("tapi boong")
 
-#sebagian besar program Python berisi banyak pernyataan.
-#Pernyataan-pernyataan tersebut dieksekusi satu per satu, sesuai urutan penulisannya.
-print("isi air panas kedalam gelas")
-print("masukkan gula")
-print("masukkan bubuk teh")
-print("dan aduk hingga merata")
-#bisa juga dibuat dalam satu baris dengan menggunakan simbol ;
-print("isi air panas kedalam gelas"); print("masukkan gula"); print("masukkan bubuk teh"); print("dan aduk hingga merata")
+#2. cara menampilkan angka pada python
+#kita dapat menggunakan fungsi print () untuk menampilkan angka.
+print(3)
+print(3)
+print(3 + 3)
+
